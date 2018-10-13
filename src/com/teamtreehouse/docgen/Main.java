aac20fdf0ac321e92551498222caf5fbf46d1d5a
MY <manojkumar1053@gmail.com>
2018-10-13 14:39:58 -0400

    Example Project for Annotation

--- src/com/teamtreehouse/docgen/Main.java
+++ src/com/teamtreehouse/docgen/Main.java
@@ -1,8 +1,37 @@
 package com.teamtreehouse.docgen;
 
+import com.teamtreehouse.math.MathUtils;
+
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
         // TODO: Process the MathUtils class's annotations
+
+        //Get a class object
+        Class<?> clazz = MathUtils.class;
+
+        //Get all the declared methods
+        Method[] methods = clazz.getMethods();
+        Method[] methods1 = clazz.getDeclaredMethods();
+
+        //Loop over method
+
+        System.out.println(" ************* All Methods  ************* ");
+        for (Method m : methods) {
+            System.out.printf("Name: %s%n", m.getName());
+            System.out.printf("\t# params:%s%n", m.getParameterCount());
+        }
+        System.out.println("--------------------------------------");
+        System.out.println("*********Declared Methods************");
+        for (Method m1 : methods1) {
+            System.out.printf("Name: %s%n", m1.getName());
+            System.out.printf("\t# params:%s%n", m1.getParameterCount());
+        }
+
+
+        //Number of params
+
 
     }
 }
--- src/com/teamtreehouse/math/MathUtils.java
+++ src/com/teamtreehouse/math/MathUtils.java
@@ -2,13 +2,31 @@ package com.teamtreehouse.math;
 
 import java.awt.geom.Point2D;
 
+import com.teamtreehouse.docgen.Doc;
+
+
+@Doc(
+        desc = "Utility Class for commonly used math function"
+)
 public class MathUtils {
     private static final Double EPSILON = 0.0001;
+
+    @Doc(
+            desc = "Calculates the area of the triangle",
+            params = {"Co-ordinates of the first vertex"},
+            returnVal = "calculated are aof the triangle"
+    )
 
     public static Double triangleArea(Point2D.Double a, Point2D.Double b, Point2D.Double c) {
         return 0.0;
     }
 
+
+    @Doc(
+            desc = "Calculates distance of the given points",
+            params = {"Coordinates of the first point", "Coordiantes of the other point"},
+            returnVal = ""
+    )
     public static Double distance(Point2D.Double a, Point2D.Double b) {
         return 0.0;
     }
@@ -16,6 +34,11 @@ public class MathUtils {
     public static Double[] quadraticRoots(int a, int b, int c) {
         return new Double[]{};
     }
+
+    @Doc(
+            desc = "Displays the values of the epsilon"
+
+    )
 
     public static void epsilon() {
 
